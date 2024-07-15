a92609244c9ed6ddc7c2c218ba5011cac41e3323
CasualDev <54014018+casualdevvv@users.noreply.github.com>
2024-07-14 20:34:50 -0400

    Add files via upload

--- modules/constants.js
+++ modules/constants.js
@@ -1,56 +1,55 @@
 // sourced these folder mappings from bloxstrap, thanks!
 
 const folderMappings = {
-    _common: {
-      'Libraries.zip': '',
-      'shaders.zip': 'shaders/',
-      'ssl.zip': 'ssl/',
-      'WebView2.zip': '',
-      'WebView2RuntimeInstaller.zip': 'WebView2RuntimeInstaller/',
-      'content-avatar.zip': 'content/avatar/',
-      'content-configs.zip': 'content/configs/',
-      'content-fonts.zip': 'content/fonts/',
-      'content-models.zip': 'content/models/',
-      'content-sky.zip': 'content/sky/',
-      'content-sounds.zip': 'content/sounds/',
-      'content-textures2.zip': 'content/textures/',
-      'content-textures3.zip': 'PlatformContent/pc/textures/',
-      'content-terrain.zip': 'PlatformContent/pc/terrain/',
-      'content-platform-fonts.zip': 'PlatformContent/pc/fonts/',
-      'extracontent-places.zip': 'ExtraContent/places/',
-      'extracontent-luapackages.zip': 'ExtraContent/LuaPackages/',
-      'extracontent-translations.zip': 'ExtraContent/translations/',
-      'extracontent-models.zip': 'ExtraContent/models/',
-      'extracontent-textures.zip': 'ExtraContent/textures/',
-    },
-    _playerOnly: {
-      'RobloxApp.zip': '',
-    },
-    _studioOnly: {
-      'RobloxStudio.zip': '',
-      'ApplicationConfig.zip': 'ApplicationConfig/',
-      'content-studio_svg_textures.zip': 'content/studio_svg_textures/',
-      'content-qt_translations.zip': 'content/qt_translations/',
-      'content-api-docs.zip': 'content/api_docs/',
-      'extracontent-scripts.zip': 'ExtraContent/scripts/',
-      'BuiltInPlugins.zip': 'BuiltInPlugins/',
-      'BuiltInStandalonePlugins.zip': 'BuiltInStandalonePlugins/',
-      'LibrariesQt5.zip': '',
-      'Plugins.zip': 'Plugins/',
-      'Qml.zip': 'Qml/',
-      'StudioFonts.zip': 'StudioFonts/',
-      'redist.zip': '',
-    },
-  };
-  
-  const AppSettings = `<?xml version="1.0" encoding="UTF-8"?>
-  <Settings>
-    <ContentFolder>content</ContentFolder>
-    <BaseUrl>http://www.roblox.com</BaseUrl>
-  </Settings>`;
-  
-  module.exports = {
-    folderMappings,
-    AppSettings
-  };
-  
+  _common: {
+    'Libraries.zip': '',
+    'shaders.zip': 'shaders/',
+    'ssl.zip': 'ssl/',
+    'WebView2.zip': '',
+    'WebView2RuntimeInstaller.zip': 'WebView2RuntimeInstaller/',
+    'content-avatar.zip': 'content/avatar/',
+    'content-configs.zip': 'content/configs/',
+    'content-fonts.zip': 'content/fonts/',
+    'content-models.zip': 'content/models/',
+    'content-sky.zip': 'content/sky/',
+    'content-sounds.zip': 'content/sounds/',
+    'content-textures2.zip': 'content/textures/',
+    'content-textures3.zip': 'PlatformContent/pc/textures/',
+    'content-terrain.zip': 'PlatformContent/pc/terrain/',
+    'content-platform-fonts.zip': 'PlatformContent/pc/fonts/',
+    'extracontent-places.zip': 'ExtraContent/places/',
+    'extracontent-luapackages.zip': 'ExtraContent/LuaPackages/',
+    'extracontent-translations.zip': 'ExtraContent/translations/',
+    'extracontent-models.zip': 'ExtraContent/models/',
+    'extracontent-textures.zip': 'ExtraContent/textures/',
+  },
+  _playerOnly: {
+    'RobloxApp.zip': '',
+  },
+  _studioOnly: {
+    'RobloxStudio.zip': '',
+    'ApplicationConfig.zip': 'ApplicationConfig/',
+    'content-studio_svg_textures.zip': 'content/studio_svg_textures/',
+    'content-qt_translations.zip': 'content/qt_translations/',
+    'content-api-docs.zip': 'content/api_docs/',
+    'extracontent-scripts.zip': 'ExtraContent/scripts/',
+    'BuiltInPlugins.zip': 'BuiltInPlugins/',
+    'BuiltInStandalonePlugins.zip': 'BuiltInStandalonePlugins/',
+    'LibrariesQt5.zip': '',
+    'Plugins.zip': 'Plugins/',
+    'Qml.zip': 'Qml/',
+    'StudioFonts.zip': 'StudioFonts/',
+    'redist.zip': '',
+  },
+};
+
+const AppSettings = `<?xml version="1.0" encoding="UTF-8"?>
+<Settings>
+  <ContentFolder>content</ContentFolder>
+  <BaseUrl>http://www.roblox.com</BaseUrl>
+</Settings>`;
+
+module.exports = {
+  folderMappings,
+  AppSettings
+};
--- modules/extract.js
+++ modules/extract.js
@@ -1,24 +1,30 @@
 const fs = require('fs');
-const unzipper = require('unzipper');
 const path = require('path');
+const AdmZip = require('adm-zip');
 
 const extractZip = (filePath, extractTo, mappings) => {
   const fileName = path.basename(filePath);
-
+  
   let type = '_common';
   if (mappings._playerOnly[fileName]) {
     type = '_playerOnly';
   } else if (mappings._studioOnly[fileName]) {
     type = '_studioOnly';
   }
-
+  
   const folderMapping = mappings[type][fileName] || '';
-
   const extractPath = path.join(extractTo, folderMapping);
-  return fs.createReadStream(filePath)
-    .pipe(unzipper.Extract({ path: extractPath }))
-    .promise()
-    .then(() => extractPath);
+
+  return new Promise((resolve, reject) => {
+    try {
+      const zip = new AdmZip(filePath);
+      zip.extractAllTo(extractPath, /*overwrite*/ true);
+
+      resolve(extractPath);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 module.exports = extractZip;
